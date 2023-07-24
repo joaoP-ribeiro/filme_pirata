1b384e88e4625a19383447a08edf9b9f9c882f54
joaoP-ribeiro <joao06.ribeiro.costa@gmail.com>
2023-07-24 07:11:11 -0300

    updt

--- src/pages/movie.jsx
+++ src/pages/movie.jsx
@@ -2,6 +2,13 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSearchParams } from "react-router-dom";
 import Nav_bar from "../components/Nav_bar.jsx";
+import Carta_filme from "../components/Carta_filme.jsx";
+import {
+    BsGraphUp,
+    BsWallet2,
+    BsHourglassSplit,
+    BsFillFileEarmarkTextFill,
+  } from "react-icons/bs";
 import "./movie.css"
 
     const filme_Url = import.meta.env.VITE_API
@@ -39,14 +46,39 @@ export default function Movie(){
 
     return(
         <div className="App_inf">
+            <Nav_bar/>
             <div className="container_inf">
                 <div>{filme && (
                     <>
-                        <div className="inf_title_filme">{filme.title}</div>
-                    
-                    
-                    
-                    
+                        <div className="card"><Carta_filme filme={filme} showLink={false}/></div>
+
+                        <div className="inf">
+                            <p className="tagline">{filme.title}</p>
+                            <div className="info">
+                                <h3>
+                                <BsWallet2 /> Orçamento:
+                                </h3>
+                                <p>{formatCurrency(filme.budget)}</p>
+                            </div>
+                            <div className="info">
+                                <h3>
+                                <BsGraphUp /> Receita:
+                                </h3>
+                                <p>{formatCurrency(filme.revenue)}</p>
+                            </div>
+                            <div className="info">
+                                <h3>
+                                <BsHourglassSplit /> Duração:
+                                </h3>
+                                <p>{filme.runtime} minutos</p>
+                            </div>
+                            <div className="info description">
+                                <h3>
+                                <BsFillFileEarmarkTextFill /> Descrição:
+                                </h3>
+                                <p>{filme.overview}</p>
+                            </div>
+                        </div>
                     </>
                 
                 )}</div>
